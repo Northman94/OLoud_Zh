0ad5615d7e55b11df8193254bd61d742f0229b17
Northman94 <Zhandarm2000@gmail.com>
2021-04-01 21:45:46 +0300

    Scene controller script creation

--- Assets/Scripts_C#/CardInteractionScript.cs
+++ Assets/Scripts_C#/CardInteractionScript.cs
@@ -5,7 +5,27 @@ using UnityEngine;
 public class CardInteractionScript : MonoBehaviour
 {
     [SerializeField]
+    private SceneControllerScript controllerScript;
+
+    [SerializeField]
     private GameObject cardBack; //Child Obj to cover Face of a Card
+
+    private int _id;
+
+
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+ 
 
     public void OnMouseDown()
     {
