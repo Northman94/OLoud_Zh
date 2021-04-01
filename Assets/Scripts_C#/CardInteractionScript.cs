e8b4ecdfbc05888135e00ef68a3cdf3fbc0d96ce
Northman94 <Zhandarm2000@gmail.com>
2021-04-01 21:20:14 +0300

    Card Back revealing Card Face - onClick()

--- Assets/Scripts_C#/CardInteractionScript.cs
+++ Assets/Scripts_C#/CardInteractionScript.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class CardInteractionScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject cardBack; //Child Obj to cover Face of a Card
+
     public void OnMouseDown()
     {
         Debug.Log("Card was Clicked On!");
+
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
     }
 }
