9659efeee916605262a581addb93646bda290f55
Northman94 <Zhandarm2000@gmail.com>
2021-04-01 21:09:26 +0300

    Cards to Scene & Card interaction Script

---
+++ Assets/Scripts_C#/CardInteractionScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInteractionScript : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("Card was Clicked On!");
+    }
+}
