120ae47f2819c7371673b38f37dcf257c8eee6f7
Northman94 <Zhandarm2000@gmail.com>
2021-04-04 14:33:13 +0300

    Array of CardFace sprites

--- Assets/Scripts_C#/SceneControllerScript.cs
+++ Assets/Scripts_C#/SceneControllerScript.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class SceneControllerScript : MonoBehaviour
 {
-   
+    [SerializeField]
+    private CardInteractionScript oiginalCArd; //Card in a Scene reference
+
+    [SerializeField]
+    private Sprite[] images; // Array of Links to Sprites
+
+
+    private void Start()
+    {
+        int id = Random.Range(0, images.Length);
+
+        oiginalCArd.SetCard(id, images[id]);
+    }
 }
