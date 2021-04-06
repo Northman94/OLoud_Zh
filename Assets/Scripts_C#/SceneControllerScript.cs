31ee3b9581d705f37fd58c87af278e3533db9354
Northman94 <Zhandarm2000@gmail.com>
2021-04-06 22:23:31 +0300

    Randomizing Cards & Placing on the Grid
    To Add:
    + no limit for creation more than two same cards

--- Assets/Scripts_C#/SceneControllerScript.cs
+++ Assets/Scripts_C#/SceneControllerScript.cs
@@ -1,23 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 //Should be attached to SceneController Obj
 public class SceneControllerScript : MonoBehaviour
 {
-    [SerializeField]
-    private CardInteractionScript oiginalFaceCard; //CardFace in a Scene reference, with relevant Script on it
+    public const int gridColumns = 4;
+    public const int gridRows = 2;
+
+    public const float offset_X = 2f;
+    public const float offset_Y = 2.5f;
 
 
+
     [SerializeField]
-    private Sprite[] images; // Array of Links to Sprites
+    private CardInteractionScript originalFaceCard; //CardFace in a Scene reference, with relevant Script on it
+
+    [SerializeField]
+    private Sprite[] images; // Array of Links to Sprite Faces
+
+
 
     private void Start()
     {
-        int id = Random.Range(0, images.Length);
+        Vector3 startPos = originalFaceCard.transform.position;
 
-        oiginalFaceCard.SetCard(id, images[id]);
+
+        for (int i = 0; i < gridColumns; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+
+                CardInteractionScript card;
+
+                if (i == 0 && j == 0)
+                {
+                    card = originalFaceCard;
+                }
+                else
+                {
+                    card = Instantiate(originalFaceCard) as CardInteractionScript;
+
+                }
+
+                //Setting a random card
+                int id = Random.Range(0, images.Length);
+                card.SetCard(id, images[id]);
+
+
+                // Moving it due on grid
+                float posX = (offset_X * i) + startPos.x;
+                float posY = (offset_Y * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+
+
+
+
+
+        }
     }
+
 
 
 }
